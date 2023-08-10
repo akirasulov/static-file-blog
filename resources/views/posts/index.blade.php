6952538b3c2b4ae9cfc9a50c3c89b398e62b1101
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-10 13:21:06 +0500

    Change the parser for added detail

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,14 @@
             @foreach ($posts as $post)
                 <article class="prose">
                     <h1 class="not-prose">
-                        <a href="" class="hover:text-blue-500 transition-colors duration-100">Blog post
-                            {{ $post->slug }}</a>
+                        <a href="" class="hover:text-blue-500 transition-colors duration-100">
+                            {{ $post->title }}
+                        </a>
                     </h1>
-                    <div>Teaser</div>
+                    <div>{{ $post->teaser }}</div>
+                    <div class="text-sm mt-10">
+                        {{ $post->author }} / {{ $post->date->toDateString() }}
+                    </div>
                 </article>
             @endforeach
         @else
