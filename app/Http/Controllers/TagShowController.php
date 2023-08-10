c127a20a9681f938f638c9a7b7f2bdbac0d5133d
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-10 14:29:57 +0500

    List blog posts under a tag

---
+++ app/Http/Controllers/TagShowController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Spatie\Sheets\Facades\Sheets;
+
+class TagShowController extends Controller
+{
+    public function __invoke(string $tag)
+    {
+        $posts = Sheets::collection('posts')
+            ->all()
+            ->filter(function (Post $post) use ($tag) {
+                return in_array($tag, $post->tags);
+            });
+
+        abort_if($posts->isEmpty(), 404);
+
+        return view('tags.show', [
+            'tag' => $tag,
+            'posts' => $posts,
+        ]);
+    }
+}
--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -4,7 +4,7 @@
         <ul class="not-prose mt-2 p-0 list-none flex items-center space-x-1">
             @foreach ($post->tags as $tag)
                 <li class="text-sm">
-                    <a class="text-blue-500" href="">{{ Str::title($tag) }}</a>
+                    <a class="text-blue-500" href="{{ route('tags.show', $tag) }}">{{ Str::title($tag) }}</a>
                 </li>
             @endforeach
         </ul>
