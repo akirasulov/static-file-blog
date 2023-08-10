207cff85a423698e00734138fce1c7fb2ea53e5d
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-10 14:08:03 +0500

    add posts tag

--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -1,3 +1,12 @@
 <div class="text-sm mt-10">
     {{ $post->author }} / {{ $post->date->toDateString() }}
+    @if (count($post->tags))
+        <ul class="not-prose mt-2 p-0 list-none flex items-center space-x-1">
+            @foreach ($post->tags as $tag)
+                <li class="text-sm">
+                    <a class="text-blue-500" href="">{{ Str::title($tag) }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 </div>
