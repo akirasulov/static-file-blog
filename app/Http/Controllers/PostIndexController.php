5e221bd4590e37a43ce1b66a5ff0b6deae7b6a6d
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-10 12:50:40 +0500

    add Sheets

--- app/Http/Controllers/PostIndexController.php
+++ app/Http/Controllers/PostIndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Spatie\Sheets\Facades\Sheets;
 
 class PostIndexController extends Controller
 {
     public function __invoke()
     {
+        dd(Sheets::collection('posts')->all());
+
         return view('posts.index');
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -11,7 +11,7 @@ return [
     | by the framework. The "local" disk, as well as a variety of cloud
     | based disks are available to your application. Just store away!
     |
-    */
+     */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
 
@@ -26,9 +26,15 @@ return [
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
-    */
+     */
 
     'disks' => [
+
+        'posts' => [
+            'driver' => 'local',
+            'root' => storage_path('app/posts'),
+            'throw' => false,
+        ],
 
         'local' => [
             'driver' => 'local',
@@ -67,7 +73,7 @@ return [
     | `storage:link` Artisan command is executed. The array keys should be
     | the locations of the links and the values should be their targets.
     |
-    */
+     */
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
